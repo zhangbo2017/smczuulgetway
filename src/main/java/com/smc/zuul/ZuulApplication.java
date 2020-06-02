92630dcc251d30ee816932fd1768c6c5a1fb0753
Bo Zhang <dlzbo@cn.ibm.com>
2020-06-02 14:33:17 +0800

    add enable eureka

--- src/main/java/com/smc/zuul/ZuulApplication.java
+++ src/main/java/com/smc/zuul/ZuulApplication.java
@@ -2,16 +2,16 @@ package com.smc.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
-@EnableDiscoveryClient
 @EnableZuulProxy
 //@EnableFeignClients(basePackages = "com.ibm.zuul.feigin")
 @EnableFeignClients
 @SpringBootApplication
+@EnableEurekaClient
 public class ZuulApplication {
 
 	public static void main(String[] args) {
